996ef2272c84583b244aa19816ab81a31ced599c
joseeduardohg <101235315+joseeduardohg@users.noreply.github.com>
2025-04-18 23:28:44 -0500

    refactor(api): remove unecessary jwt secret from auth guard

--- api/src/auth/guards/auth.guard.ts
+++ api/src/auth/guards/auth.guard.ts
@@ -4,29 +4,22 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 import { JwtPayload } from '../types/payload';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(
-    private configService: ConfigService,
-    private jwtService: JwtService,
-  ) {}
+  constructor(private jwtService: JwtService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest<Request>();
     const token = this.extractTokenFromHeader(request);
-    if (!token) throw new UnauthorizedException('No token provided');
-    try {
-      const payload = await this.jwtService.verifyAsync<JwtPayload>(token, {
-        secret: this.configService.get('JWT_SECRET'),
-      });
 
-      // Attach the payload to the request object
-      // so that it can be accessed in the route handler
+    if (!token) throw new UnauthorizedException('No token provided');
+
+    try {
+      const payload = await this.jwtService.verifyAsync<JwtPayload>(token);
       request['user'] = payload;
     } catch {
       throw new UnauthorizedException('Invalid token');
