33876384fa96dd5315e6f6dfb1adf551529fa8eb
joseeduardohg <101235315+joseeduardohg@users.noreply.github.com>
2025-04-18 19:02:39 -0500

    feat(api): setup environment variables vaildation with database and jwt secret

---
+++ api/src/env.validation.ts
@@ -0,0 +1,37 @@
+import { plainToInstance } from 'class-transformer';
+import { IsInt, IsNotEmpty, IsString, validateSync } from 'class-validator';
+
+class EnvironmentVariables {
+  @IsString()
+  @IsNotEmpty()
+  DATABASE_HOST: string;
+
+  @IsInt()
+  DATABASE_PORT: number;
+
+  @IsString()
+  @IsNotEmpty()
+  DATABASE_USERNAME: string;
+
+  @IsString()
+  @IsNotEmpty()
+  DATABASE_PASSWORD: string;
+
+  @IsString()
+  @IsNotEmpty()
+  DATABASE_NAME: string;
+
+  @IsString()
+  @IsNotEmpty()
+  JWT_SECRET: string;
+}
+
+export function validate(config: Record<string, unknown>) {
+  const validateConfig = plainToInstance(EnvironmentVariables, config, {
+    enableImplicitConversion: true,
+  });
+  const errors = validateSync(validateConfig, { skipMissingProperties: false });
+  if (errors.length > 0) throw new Error(errors.toString());
+
+  return validateConfig;
+}
